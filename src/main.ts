8d69937f71e6b2c42f60ad6b42209a84ebf8bd15
jreinke428 <reinkejace@gmail.com>
2024-03-22 23:04:05 -0500

    init primevue

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
 import App from './App.vue';
 import router from './router';
 
-import 'tailwindcss/base.css';
-import 'tailwindcss/components.css';
-import 'tailwindcss/utilities.css';
+import './styles.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
+app.use(PrimeVue);
+app.use(ToastService);
+app.directive('tooltip', Tooltip);
 app.mount('#app');
